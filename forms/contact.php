be1e48326596dbb84f2ae81b6f30124ae4ad9eea
Abhijeet <sabhi8400@gmail.com>
2025-06-23 11:00:36 +0530

    udtae-contact

--- forms/contact.php
+++ forms/contact.php
@@ -26,9 +26,9 @@
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
+    'host' => 'smtp.gmail.com',
+    'username' => 'vedantam.ayurveda.mailer@gmail.com',
+    'password' => 'qekuykbefdgwfdf',
     'port' => '587'
   );
   
